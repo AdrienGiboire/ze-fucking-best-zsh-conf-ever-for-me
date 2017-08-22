01824dff7585a67ad5a03367d7c502249716f5bc
Adrien Giboire <adrien+github@sent.com>
2017-08-22 21:32:05 +0200

    improv: only check ruby version at chpwd

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -5,20 +5,25 @@ function git_branch {
   fi
 }
 
-function ruby_version {
+function _ruby_version {
   if [ -e .ruby-version ]; then
     local ruby_version="$(cat .ruby-version)"
     echo " (${PR_YELLOW}${ruby_version}%{$reset_color%})"
   fi
 }
 
+typeset -g ruby_version="$(_ruby_version)"
 typeset -g current_pwd="$(pwd | sed -e "s,^$HOME,~,")"
 
 function _update_current_pwd {
   current_pwd="$(pwd | sed -e "s,^$HOME,~,")"
 }
 
+function _update_ruby_version {
+  ruby_version="$(_ruby_version)"
+}
+
 chpwd_functions+=(_update_ruby_version _update_current_pwd)
 PROMPT='
-${PR_GREEN}${current_pwd}%{$reset_color%}$(ruby_version)$(git_branch)
+${PR_GREEN}${current_pwd}%{$reset_color%}${ruby_version}$(git_branch)
 > '
