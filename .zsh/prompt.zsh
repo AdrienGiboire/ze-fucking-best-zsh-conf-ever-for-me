d28daf5c4dcb948246675355039b72753622ae74
Adrien Giboire <adrien+github@sent.com>
2017-08-22 10:42:10 +0200

    improv: prompt display

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,14 +1,14 @@
 function git_branch {
   if [ -e .git ]; then
     local current_branch="$(git b | grep -e '*' | cut -d ' ' -f2)"
-    echo "(${PR_BOLD_BLUE}${current_branch}%{$reset_color%})"
+    echo " (${PR_BOLD_BLUE}${current_branch}%{$reset_color%})"
   fi
 }
 
 function ruby_version {
   if [ -e .ruby-version ]; then
     local ruby_version="$(cat .ruby-version)"
-    echo "(${PR_YELLOW}${ruby_version}%{$reset_color%})"
+    echo " (${PR_YELLOW}${ruby_version}%{$reset_color%})"
   fi
 }
 
@@ -20,5 +20,5 @@ function _update_current_pwd {
 
 chpwd_functions+=(_update_ruby_version _update_current_pwd)
 PROMPT='
-${PR_BOLD_GREEN}${current_pwd}%{$reset_color%} $(ruby_version) $(git_branch)
+${PR_BOLD_GREEN}${current_pwd}%{$reset_color%}$(ruby_version)$(git_branch)
 > '
